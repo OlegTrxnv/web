72ac812d2ede150734dd3612fbeca48e4b84697f
Oleg Trxnv <oleg.tarkhanov@gmail.com>
2021-03-19 17:34:05 +0200

    Implement collection view
    - create abstract CollectionView class
    - create UserList extending CollectionView

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,22 @@
-import { User } from "./models/User";
-import { UserEdit } from "./views/UserEdit";
+import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
+import { UserList } from "./views/UserList";
 
-const user = User.buildUser({ name: "New user", age: 30 });
+const users = new Collection(
+  "http://localhost:3000/users",
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
 
-const root = document.querySelector("#root");
+users.on("change", () => {
+  const root = document.getElementById("root");
 
-if (root) {
-  const userEdit = new UserEdit(root, user);
-  userEdit.render();
-  console.log(userEdit);
-} else {
-  throw new Error("No root element found");
-}
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
 
-// Generating tsconfig file turns on Strict Type-Checking Options {default behavior is "off")
+console.log(users);
+
+users.fetch();
